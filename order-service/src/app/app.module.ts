c08af2bc6baade40db5a9a8a57d31335ccb2f801
Memona Amir <157165172+MemonaAmirAbdulHaq@users.noreply.github.com>
2026-08-07 22:41:56 +0500

    refactor: update AppModule to use gRPC for inventory service

--- order-service/src/app/app.module.ts
+++ order-service/src/app/app.module.ts
@@ -1,23 +1,50 @@
+//=========SERVICE MODULES FOR AUTHENTICATION SERVICE=========
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// import { ClientsModule, Transport } from '@nestjs/microservices';
+
+
+// @Module({
+//   imports: [
+//     ClientsModule.register([
+//       {
+//         name: 'AUTH_SERVICE',
+//         transport: Transport.TCP,
+//         options: {
+//           host: '127.0.0.1',
+//           port:5005,
+//         }
+//       }
+//     ])
+//   ],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
+
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 
 @Module({
   imports: [
     ClientsModule.register([
       {
-        name: 'AUTH_SERVICE',
-        transport: Transport.TCP,
+        name: 'INVENTORY_PACKAGE',
+        transport: Transport.GRPC,
         options: {
-          host: '127.0.0.1',
-          port:5005,
+          package: 'inventory',
+          protoPath:join(process.cwd(),'libs/proto/inventory.proto'),
         }
       }
     ])
   ],
   controllers: [AppController],
-  providers: [AppService],
+  
 })
 export class AppModule {}
+
