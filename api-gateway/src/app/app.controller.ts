f7e94b17402da7d08fdf752a3032e45efeb7cab3
Memona Amir <157165172+MemonaAmirAbdulHaq@users.noreply.github.com>
2026-08-23 15:55:23 +0500

    feat: integrate Kafka service for user creation event publishing

--- api-gateway/src/app/app.controller.ts
+++ api-gateway/src/app/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Inject, OnModuleInit } from "@nestjs/common";
+
+import { ClientKafka } from "@nestjs/microservices";
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+export class AppController implements OnModuleInit{
+  constructor(
+    @Inject('KAFKA_SERVICE')
+    private readonly kafkaClient: ClientKafka,
+  ) {};
 
-  @Get()
-  getData() {
-    return this.appService.getData();
+  async onModuleInit() {
+    await this.kafkaClient.connect();
+  }
+  @Get('create-user')
+  async createUser() {
+    const user = {
+      id: 1,
+      name: 'MEmona',
+      email: 'amirmemona6@gmail.com'
+    };
+    this.kafkaClient.emit('user_created', user);
+    return {
+      message: 'User Created Event Published!',
+      user
+    }
   }
 }
--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,21 +1,11 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app/app.module";
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
-
-async function bootstrap() {
+async function bootstrap(){
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-  );
+  await app.listen(3000);
+
+  console.log('API Gateway is Running!')
 }
 
 bootstrap();
