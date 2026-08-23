323dea4adf123443784da5cc19d897406763fba0
Memona Amir <157165172+MemonaAmirAbdulHaq@users.noreply.github.com>
2026-08-23 19:24:55 +0500

    feat: implement DLQ integration in AppController and AppModule

--- notification-service/src/app/app.controller.ts
+++ notification-service/src/app/app.controller.ts
@@ -1,15 +1,59 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Inject, OnModuleInit } from "@nestjs/common";
 
-import {  EventPattern, Payload } from "@nestjs/microservices";
+import { ClientKafka, EventPattern, Payload } from "@nestjs/microservices";
 
 @Controller()
-export class AppController {
-  @EventPattern('user_created')
-   handleUserCreated(@Payload() data: any){
-   
-      console.log('Main Event Recieved!')
-      console.log(data)  
-    
+export class AppController implements OnModuleInit{
+  constructor(
+    @Inject('KAFKA_SERVICE')
+    private readonly kafkaClient : ClientKafka,
+  ) {};
+  async onModuleInit(){
+    await this.kafkaClient.connect();
   }
-  
+  @EventPattern('user_created')
+  async handleUserCreated(@Payload() data: any){
+    try {
+      console.log('Main Event Recieved!')
+      console.log(data);
+      throw new Error('Payment Service Failed!');
+    } catch (error: unknown) {
+      const errorMessage = error instanceof Error ? error.message : String(error);
+      console.log('Sending Event to DLQ');
+      this.kafkaClient.emit('user_created_dlq', {
+        failedData: data,
+        error: errorMessage,
+        failedAt: new Date()
+      })
+    }
+  }
+  @EventPattern('user_created_dlq')
+  handleDLQ(@Payload() data: any){
+    console.log('dlq event received!')
+    console.log(data)
+  }
 }
+
+
+
+
+
+
+
+
+//============Code for Implement Kafka ================
+// import { Controller } from "@nestjs/common";
+
+// import {  EventPattern, Payload } from "@nestjs/microservices";
+
+// @Controller()
+// export class AppController {
+//   @EventPattern('user_created')
+//    handleUserCreated(@Payload() data: any){
+   
+//       console.log('Main Event Recieved!')
+//       console.log(data)  
+    
+//   }
+  
+// }
