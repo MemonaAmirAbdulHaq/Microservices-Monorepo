c2c10520afc32be85afbb41512c5d58a71cfcbba
Memona Amir <157165172+MemonaAmirAbdulHaq@users.noreply.github.com>
2026-08-24 23:37:10 +0500

    add RateLimitGuard to manage request limits

---
+++ product-service/guards/rate-limit.guard.ts
@@ -0,0 +1,30 @@
+import { CanActivate, ExecutionContext, Injectable, BadRequestException } from "@nestjs/common";
+
+import { RedisService } from "../src/redis/redis.serviceice";
+
+@Injectable()
+export class RateLimitGuard implements CanActivate{
+    constructor(
+        private readonly redisService: RedisService,
+    ) {}
+
+    async canActivate(context: ExecutionContext): | Promise<boolean>{
+        const request = context.switchToHttp().getRequest();
+        const ip = request.ip || 'unkown';
+        const key = `rate_limit:${ip}`
+
+        const currentRequests = await this.redisService.client.incr(key);
+        if(currentRequests === 1){
+            await this.redisService.client.expire(
+                key,
+                60
+            )
+        }
+        if(currentRequests > 5){
+            throw new BadRequestException(
+                'Too many requests! Try again later...'
+            )
+        }
+        return true;
+    }
+}
