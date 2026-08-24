76e08132e97e29660e62c21d8750a4b2e2399a72
Memona Amir <157165172+MemonaAmirAbdulHaq@users.noreply.github.com>
2026-08-24 23:37:24 +0500

    implement Redis service for connection management

--- product-service/src/app/app.controller.ts
+++ product-service/src/app/app.controller.ts
@@ -1,12 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { RateLimitGuard } from '../guards/rate-limit.guard';
+import { Controller, Get, Param, Patch, UseGuards } from "@nestjs/common";
+
+import { AppService } from "./app.service";
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getData() {
-    return this.appService.getData();
+export class AppController{
+  constructor(
+    private readonly appService: AppService
+  ) {}
+  @Get('product/:id')
+  async getProduct(@Param('id') id: string){
+    return this.appService.getProduct(Number(id))
+  }
+  @Patch('product/:id/:price')
+  async updateProduct(
+    @Param('id') id: string,
+    @Param('price') price: string,
+  ){
+    return this.appService.updateProduct(Number(id), Number(price))
+  }
+  @Get('products')
+  @UseGuards(
+    RateLimitGuard
+  )
+  getProducts(){
+    return{
+      success: true,
+      products: [
+        'Laptop',
+        'Mobile',
+        'Tablets'
+      ]
+    }
   }
 }
+
+
+
---
+++ product-service/src/redis/redis.service.ts
@@ -0,0 +1,14 @@
+import {Injectable, OnModuleInit} from '@nestjs/common';
+import { createClient } from 'redis';
+
+@Injectable()
+export class RedisService implements OnModuleInit{
+    public client = createClient({
+        url: 'redis://localhost:6379'
+    })
+    async onModuleInit() {
+        await this.client.connect();
+        console.log('Redis Connected Successfully!')
+    }
+    
+}
